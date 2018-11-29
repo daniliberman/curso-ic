7df1e55ff45afe0c51f69131b5aefb6b8977d90e
DANIELLA LIBERMAN <dliberman@ar0fvfxf1a9hv2j.ml.com>
2018-11-29 11:37:46 -0300

    linkeo redis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker stop curso
 docker rm curso
-docker run --link sql_server:sql_server --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
+
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0_191`
+docker run --link sql-server:sql-server --link some-redis:some-redis --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
